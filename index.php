2357c513a32c3355ad3e154c5459be2509550dbc
Carlo Minucci <gecco@napodano.com>
2015-06-22 09:36:54 +0200

    aggiunta alla ricerca la foto con l'anteprima del video

--- index.php
+++ index.php
@@ -13,40 +13,58 @@ if(isset($_POST[keyword])){
 	$result = curl_exec ($curl);
 	curl_close ($curl);
 	$array=split("yt-lockup-content", $result);
+	$arrimg=split("yt-thumb video-thumb", $result);
 	$arr1=split("\"", $array[3]);
 	$arr2=split("\"", $array[4]);
 	$arr3=split("\"", $array[5]);
 	$arr4=split("\"", $array[6]);
 	$arr5=split("\"", $array[7]);
+	
+	$img1=split("\"", $arrimg[1]);
+	$img2=split("\"", $arrimg[2]);
+	$img3=split("\"", $arrimg[3]);
+	$img4=split("\"", $arrimg[4]);
+	$img5=split("\"", $arrimg[5]);
+
+	
+	echo "<img src=\"http:" . $img1[2] . "\" alt=\"" . $arr1[10] . "\" /><br />";
 	echo $arr1[10];
 	$t=explode(".", $arr1[21]);
 	$t=explode(": ", $t[0]);
 	echo " - " . $t[1];
 	$tmp=split("=", $arr1[4]);
-	echo " <a href=\"add.php?add=" . $tmp[1] ."\">add</a><br />";
+	echo " <a href=\"add.php?add=" . $tmp[1] ."\">add</a><br />\n";
+	
+	echo "<img src=\"http:" . $img2[2] . "\" alt=\"" . $arr2[10] . "\" /><br />";
 	echo $arr2[10];
 	$t=explode(".", $arr2[21]);
 	$t=explode(": ", $t[0]);
 	echo " - " . $t[1];
 	$tmp=split("=", $arr2[4]);
-	echo " <a href=\"add.php?add=" . $tmp[1] ."\">add</a><br />";
+	echo " <a href=\"add.php?add=" . $tmp[1] ."\">add</a><br />\n";
+	
+	echo "<img src=\"http:" . $img3[2] . "\" alt=\"" . $arr3[10] . "\" /><br />";
 	echo $arr3[10];
 	$t=explode(".", $arr3[21]);
 	$t=explode(": ", $t[0]);
 	echo " - " . $t[1];
 	$tmp=split("=", $arr3[4]);
-	echo " <a href=\"add.php?add=" . $tmp[1] ."\">add</a><br />";
+	echo " <a href=\"add.php?add=" . $tmp[1] ."\">add</a><br />\n";
+	
+	echo "<img src=\"http:" . $img4[2] . "\" alt=\"" . $arr4[10] . "\" /><br />";
 	echo $arr3[10];
 	$t=explode(".", $arr3[21]);
 	$t=explode(": ", $t[0]);
 	echo " - " . $t[1];
 	$tmp=split("=", $arr3[4]);
-	echo " <a href=\"add.php?add=" . $tmp[1] ."\">add</a><br />";
+	echo " <a href=\"add.php?add=" . $tmp[1] ."\">add</a><br />\n";
+	
+	echo "<img src=\"http:" . $img5[2] . "\" alt=\"" . $arr5[10] . "\" /><br />";
 	echo $arr5[10];
 	$t=explode(".", $arr5[21]);
 	$t=explode(": ", $t[0]);
 	echo " - " . $t[1];
 	$tmp=split("=", $arr5[4]);
-	echo " <a href=\"add.php?add=" . $tmp[1] ."\">add</a><br />";
+	echo " <a href=\"add.php?add=" . $tmp[1] ."\">add</a><br />\n";
 }
 ?>

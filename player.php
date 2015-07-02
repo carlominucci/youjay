8deb4f6497386ffd38ad3532f8f67a5745b4c0c4
Carlo Minucci <gecco@napodano.com>
2015-07-02 14:00:07 +0200

    sistemata la routine di loop dei video e aggiunto il controllo d'errore nel caso la playlist sia vuota

--- player.php
+++ player.php
@@ -2,12 +2,16 @@
 session_start();
 if(!isset($_SESSION["id"])){
 	$_SESSION["id"] = "0";
-}else{
+}elseif(isset($_SESSION["id"])){
 	$_SESSION["id"]++;
 }
+echo "!!" . $_SESSION["id"] . "!!";
 //echo "sessionid " . $_SESSION["id"];
 $tmp=date("Ymd");
-$lines=file(sys_get_temp_dir() . "/" . $tmp . ".yjpl");
+
+if(isset($_GET["delete"]) && $_GET["delete"] = "yes"){
+	unlink(sys_get_temp_dir() . "/" . $tmp . ".yjpl");
+}
 ?>
 
 <!DOCTYPE html>
@@ -19,75 +23,76 @@ $lines=file(sys_get_temp_dir() . "/" . $tmp . ".yjpl");
 <body>
 <?php
 
-echo count($lines) . " video in coda...<br />";
-echo "video: " . ($_SESSION["id"]+1) . "<br />";
-if($_SESSION["id"] >= (count($lines)-1)){
-	session_unset();
-	session_destroy();
+
+if(file_exists(sys_get_temp_dir() . "/" . $tmp . ".yjpl")){
+	$lines=file(sys_get_temp_dir() . "/" . $tmp . ".yjpl");
+	if((count($lines)) < ($_SESSION["id"]+1)){
+		session_unset();
+		session_destroy();
+		?>
+		<script>
+			location.assign("player.php")
+		</script>
+		<?php
+	}else{
+		echo "video: " . ($_SESSION["id"]+1) . " di " . count($lines) . "<br />";
+		//echo "|" . $lines[$_SESSION["id"]] . "|<br />";
+		//print_r($lines);
+		if(count($lines) > 0){
+		?>
+			<div id="player"></div>
+			<script>
+			  var tag = document.createElement('script');
+			  tag.src = "https://www.youtube.com/iframe_api";
+			  var firstScriptTag = document.getElementsByTagName('script')[0];
+			  firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+			  var player;
+			  function onYouTubeIframeAPIReady() {
+				player = new YT.Player('player', {
+				  height: '390',
+				  width: '640',
+				  videoId: '<?php echo str_replace("\n", "", $lines[$_SESSION["id"]]); ?>',
+				  events: {
+				    'onReady': onPlayerReady,
+				    'onStateChange': onPlayerStateChange,
+				    'onError': onChangeVideo
+				  }
+				});
+			  }
+			  function onPlayerReady(event) {
+				event.target.playVideo();
+			  }
+			  var done = false;
+			  function onChangeVideo(event){
+			  	location.reload();
+			  }
+			  function onPlayerStateChange(event) {
+			  	//alert(player.getPlayerState());
+			  	alert(onError());
+			  	if (player.get.PlayerState() == -1){
+					if (player.getPlayerState() == 0){
+						//location.reload();
+						location.assign("player.php");
+					}
+				}
+				/*if (player.getPlayerState() < 0){
+					document.write("Problema con il video, passo al successivo tra 5 secondi...");
+					setTimeout("location.reload(true);", 5000);
+				}*/
+			  }
+			  function stopVideo() {
+				player.stopVideo();
+			  }
+			</script>
+			</div>
+		<?php
+		}
+	}
+}else{
+	echo "nessun video nella playlist...";
 }
-if(count($lines) > 0){
 ?>
-    <div id="player"></div>
-    <script>
-      var tag = document.createElement('script');
-      tag.src = "https://www.youtube.com/iframe_api";
-      var firstScriptTag = document.getElementsByTagName('script')[0];
-      firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
-      var player;
-      function onYouTubeIframeAPIReady() {
-        player = new YT.Player('player', {
-          height: '390',
-          width: '640',
-          videoId: '<?php echo str_replace("\n", "", $lines[$_SESSION["id"]]); ?>',
-          events: {
-            'onReady': onPlayerReady,
-            'onStateChange': onPlayerStateChange,
-            'onError': onChangeVideo
-          }
-        });
-      }
-      function onPlayerReady(event) {
-        event.target.playVideo();
-      }
-      var done = false;
-      function onChangeVideo(event){
-
-      	location.reload();
-      }
-      function onPlayerStateChange(event) {
-      	//alert(player.getPlayerState());
-      	alert(onError());
-      	if (player.get.PlayerState() == -1){
-
-        	if (player.getPlayerState() == 0){
-        		//location.reload();
-        		location.assign("player.php")
-        	}
-        }
-
-        /*if (player.getPlayerState() < 0){
-        	document.write("Problema con il video, passo al successivo tra 5 secondi...");
-        	setTimeout("location.reload(true);", 5000);
-        }*/
-      }
-      function stopVideo() {
-        player.stopVideo();
-      }
-    </script>
-<?php
-}
-?>	<br />
-	<!-- <form action="player.php" method="post">
-		Seleziona il tempo massimo per ogni video:<select name="time">
-			<option value="0" selected="true">nessuno</option>
-			<option value="1" >1 min.</option>
-			<option value="2" >2 min.</option>
-			<option value="3" >3 min.</option>
-			<option value="4" >4 min.</option>
-			<option value="5" >5 min.</option>
-			<option value="6" >6 min.</option>
-		</select>
-	</form>-->
-	<a href="player.php">Avanti...</a>
+	<br />
+	<a href="player.php">salta video...</a><br /><a href="player.php?delete=yes">cancella playlist...</a>
   </body>
 </html>

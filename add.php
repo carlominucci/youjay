fde5e4dd40d0b1b180fc4202221cd421f3f1068f
Carlo Minucci <gecco@napodano.com>
2015-09-21 12:48:24 +0200

    ora salva i dati del video correttamente nel database

--- add.php
+++ add.php
@@ -9,24 +9,15 @@
 <body>
 <?php
 
-/*$filename=$_GET[add] . ".yjpl";
-$file=fopen(sys_get_temp_dir() . "/" . $filename, "w");
-fclose($file);*/
-
 $data=date("U");
 $tmp=date("Ymd");
-//$fp=fopen(sys_get_temp_dir() . "/" . $tmp . ".yjpl", 'a');
-//fwrite($fp, $_GET["id"] . "\n");
-//fclose($fp);
-$query="INSERT INTO playlist VALUES(\"\", \"" . $_GET["id"] . "\", \"" . addslashes($_GET["title"]) . "\", \"FALSE\", \"FALSE\")";
+
+$query="INSERT INTO playlist (videoid, title, play, download) VALUES (\"" . $_GET["id"] . "\", \"" . addslashes($_GET["title"]) . "\", \"FALSE\", \"FALSE\");";
+
+$db = new SQLite3('youjay.db');
+$results = $db->query($query);
 ?>
 Traccia aggiunta alla playlist.<br />
 <a href="index.php">Torna alla ricerca</a>
 </body>
 </html>
-<?php
-echo $query;
-//$dlcommand="nohup youtube-dl http://youtube.com/watch?v=" . $_GET["add"] . " -o /tmp/" . $_GET["add"] . " > /dev/null &";
-//echo $dlcommand;
-//exec($dlcommand);
-?>

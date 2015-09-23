ce1eb0ab7206068f97f58aa437b5d45325bf8bb6
Carlo Minucci <gecco@napodano.com>
2015-09-23 13:27:07 +0200

    ora il player, se non ha più nuovi video, ne seleziona uno a random finchè non vengono aggiunti nuovi video

--- player.sh
+++ player.sh
@@ -7,8 +7,11 @@ do
 		mplayer /tmp/$video
 		echo "play brano"
 		sqlite3 youjay.db "UPDATE playlist SET play = 'TRUE' WHERE videoid = '$video';"
-	else
-		echo "nessun nuovo brano"
+	else	
+		randomid=$(sqlite3 youjay.db "SELECT id FROM playlist ORDER BY RANDOM() LIMIT 1";)
+		echo "nessun brano nuovo, ne prendo uno a caso"
+		videoold=$(sqlite3 youjay.db "SELECT videoid FROM playlist WHERE download = 'TRUE' AND id = $randomid");
+		mplayer /tmp/$videoold
 		sleep 1
 	fi
 done

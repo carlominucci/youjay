687e9396274f43a20ca04ecee3fb79787bd48f50
Carlo Minucci <gecco@napodano.com>
2015-10-05 14:03:12 +0200

    ah niente... non sono riuscito a capire come evitare il download nel caso il file esista già, ci riprovo domattina

--- download.sh
+++ download.sh
@@ -6,13 +6,10 @@ cd tmp
 while :
 do
 	download=$(sqlite3 ../youjay.db "SELECT videoid FROM playlist WHERE download = 'FALSE'" | head -1);
-	if [ ! -f $download.mp4 ]; then
-		echo "refresh..."
-	elif [ -f $download.mp4 ]; then
-		echo "scarico $download"
-		youtube-dl --id http://youtube.com/watch?v=$download
-		sqlite3 ../youjay.db "UPDATE playlist SET download = 'TRUE' WHERE videoid = '$download'"
-		echo "refresh..."
+	if [ $download ] ; then
+			youtube-dl --id http://youtube.com/watch?v=$download
+			sqlite3 ../youjay.db "UPDATE playlist SET download = 'TRUE' WHERE videoid = '$download'"
+			echo "refresh..."
 	fi
 	sleep 5;
 done

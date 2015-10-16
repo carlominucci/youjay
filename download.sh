db88acb91fcfffa7e3364daab4ea9f3f8338a268
Carlo Minucci <gecco@napodano.com>
2015-10-16 20:02:29 +0200

    ahhhhhhhhhhh di nuovogit add download.sh

--- download.sh
+++ download.sh
@@ -5,7 +5,7 @@ mkdir tmp
 cd tmp
 while :
 do
-	download=$(sqlite3 ../youjay.db "SELECT videoid FROM playlist WHERE download = 'FALSE'" | head -1);
+	download=$(sqlite3 youjay.db "SELECT videoid FROM playlist WHERE download = 'FALSE'" | head -1);
 	if [ $download ] ; then
 			youtube-dl --id http://youtube.com/watch?v=$download
 			sqlite3 youjay.db "UPDATE playlist SET download = 'TRUE' WHERE videoid = '$download'"

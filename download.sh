70f6bb49022df182c7e19029f10a3db22debc45d
Carlo Minucci <gecco@napodano.com>
2015-10-16 20:22:51 +0200

    gran casino nelle release, questa forse è la volta buona

--- download.sh
+++ download.sh
@@ -5,10 +5,10 @@ mkdir tmp
 cd tmp
 while :
 do
-	download=$(sqlite3 youjay.db "SELECT videoid FROM playlist WHERE download = 'FALSE'" | head -1);
+	download=$(sqlite3 ../youjay.db "SELECT videoid FROM playlist WHERE download = 'FALSE'" | head -1);
 	if [ $download ] ; then
 			youtube-dl --id http://youtube.com/watch?v=$download
-			sqlite3 youjay.db "UPDATE playlist SET download = 'TRUE' WHERE videoid = '$download'"
+			sqlite3 ../youjay.db "UPDATE playlist SET download = 'TRUE' WHERE videoid = '$download'"
 	fi
 	echo "refresh..."
 	sleep 5;
--- player.sh
+++ player.sh
@@ -4,14 +4,14 @@ do
 	video=$(sqlite3 youjay.db "SELECT videoid FROM playlist WHERE play = 'FALSE' AND download = 'TRUE' ORDER BY 'id';" | head -1);
 	if [ $video ]
 	then
-		mplayer --really-quiet tmp/$video.mp4 > /dev/null 2>&1
+		mplayer tmp/$video.mp4 
 		echo "play brano"
 		sqlite3 youjay.db "UPDATE playlist SET play = 'TRUE' WHERE videoid = '$video';"
 	else	
 		randomid=$(sqlite3 youjay.db "SELECT id FROM playlist ORDER BY RANDOM() LIMIT 1";)
 		echo "nessun brano nuovo, ne prendo uno a caso"
-		videoold=$(sqlite3 youjay.db "SELECT videoid FROM playlist WHERE download = 'TRUE' AND id = $randomid");
-		mplayer --really-quiet tmp/$videoold.mp4 > /dev/null 2>&1
+		videoold=$(sqlite3 youjay.db "SELECT videoid FROM playlist WHERE download = 'TRUE' AND id = '$randomid'");
+		mplayer tmp/$videoold.mp4 
 		sleep 1
 	fi
 done

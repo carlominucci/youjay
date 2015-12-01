10886539981f5fb45a1c886b7960d69143250eb9
Carlo Minucci <gecco@napodano.com>
2015-12-01 09:58:09 +0100

    aggiunto il titolo della canzone in sovraimpressione

--- download.sh
+++ download.sh
@@ -6,12 +6,16 @@ cd tmp
 while :
 do
 	download=$(sqlite3 ../youjay.db "SELECT videoid FROM playlist WHERE download = 'FALSE'" | head -1);
+	title=$(sqlite3 ../youjay.db "SELECT title FROM playlist WHERE videoid ='$download'");
+	echo "1" > $download.srt
+	echo "00:00:02,000 --> 00:00:10,000" >> $download.srt
+	echo $title >> $download.srt
 	if [ $download ] ; then
 		echo $download
 		youtube-dl --id http://youtube.com/watch?v=$download
 		sqlite3 ../youjay.db "UPDATE playlist SET download = 'TRUE' WHERE videoid = '$download'"
 	fi
-	echo "refresh..."
+	echo "   "
 	sleep 5;
 	df=$(df -h | head -2 | tail -1 | awk '{print $5}' | tr -d "%")
 	if [ $df -gt "90" ] ; then
--- player.sh
+++ player.sh
@@ -4,7 +4,7 @@ do
 	video=$(sqlite3 youjay.db "SELECT videoid FROM playlist WHERE play = 'FALSE' AND download = 'TRUE' ORDER BY 'id';" | head -1);
 	if [ $video ]
 	then
-		mplayer tmp/$video.mp4 
+		mplayer tmp/$video.mp4 -sub tmp/video.srt
 		echo "play brano"
 		sqlite3 youjay.db "UPDATE playlist SET play = 'TRUE' WHERE videoid = '$video';"
 	else	
